a3768641f96cf5f137019868ecf0f05a7b1bd64f
kakumans <christiankaku1997@gmail.com>
2023-11-10 18:46:58 +0100

    create a test api route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Signin from './pages/Signin';
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
@@ -16,4 +17,6 @@ const app = express();
 app.listen(3000,() => {
     console.log('server is listening on port 3000...');
 })
+
+app.use('/api/user', userRouter)
 
